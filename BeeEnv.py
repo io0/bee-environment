b935c4b892db230c61f401bfe943ecc04d8542af
Marley <marleyxiong0@gmail.com>
2020-04-30 13:07:20 -0400

    signaling logic

--- BeeEnv.py
+++ BeeEnv.py
@@ -25,6 +25,9 @@ VIEWPORT_H = 600
 
 SCALE_X = VIEWPORT_W/GRID[0]
 SCALE_Y = VIEWPORT_H/GRID[1]
+def mdist(a, b, radius):
+    # Manhattan distance
+    return np.abs(a - b).sum() < radius
 
 class BeeEnv(gym.Env, EzPickle):
     def __init__(self, 
@@ -32,7 +35,8 @@ class BeeEnv(gym.Env, EzPickle):
                  max_episode_steps = 200,
                  logging=True,
                  fov=True,
-                 signaling=True,
+                 enable_signaling=True,
+                 n_frames = 3,
                  hive=HIVE):
         self.directions = np.array([
             [-1,0],  #left
@@ -43,10 +47,14 @@ class BeeEnv(gym.Env, EzPickle):
             ])
         self.viewer = None
         self.n_agents = n_agents
+        self.n_movements = 5
         self.action_space = [spaces.Discrete(5) for i in range(self.n_agents)]
+        if enable_signaling:
+            self.action_space[0] = spaces.Discrete(10)
         self.fov = fov
-        self.fov_buffer = [[0,0]]*3
-        self.enable_signaling = signaling
+        self.fov_buffer = [[0,0]]*n_frames
+        self.n_frames = n_frames
+        self.enable_signaling = enable_signaling
         self.logging = logging
         self.flower = np.array([0,0])
         self.hive = hive
@@ -57,7 +65,7 @@ class BeeEnv(gym.Env, EzPickle):
         
     def _place_flower(self):
         flower = np.array([np.random.randint(GRID[0]), np.random.randint(GRID[0])])
-        while (np.abs(flower - self.hive).sum() < 7):    
+        while (mdist(self.hive, flower, 7)):    
             flower = np.array([np.random.randint(GRID[0]), np.random.randint(GRID[0])])
         self.flower = np.clip(flower, self.min_position, self.max_position)
     
@@ -66,6 +74,11 @@ class BeeEnv(gym.Env, EzPickle):
         # dir_ = dir_ / np.linalg.norm(dir_) # to unit vector
         dir_ = self.directions[np.argmin(cdist([dir_],self.directions),1)] 
         return dir_
+    def _reset_fov_buffer(self):
+        self.fov_buffer = [[0,0]]*self.n_frames
+        
+    def _in_hive(self, idx):
+        return mdist(self.positions[idx], self.hive, HIVE_RADIUS)
     
     def _compute_state(self):
         coords = np.zeros([self.n_agents] + GRID)
@@ -73,17 +86,31 @@ class BeeEnv(gym.Env, EzPickle):
             coords[(idx,pos[0], pos[1])] = 1
         coords = np.reshape(coords, (self.n_agents, -1))
         state = []
+        lead_hive = self._in_hive(0)
         if self.fov:
             fov = self._compute_fov()[0]
-            self.fov_buffer.append(fov)
-            self.fov_buffer.pop(0)
-            state.append(np.hstack((coords[0],fov)))
+            if lead_hive:
+                self.fov_buffer.append(fov)
+                self.fov_buffer.pop(0)
+            else:
+                self._reset_fov_buffer()
+            if self.enable_signaling:
+                signaling = 0
+                if self.is_signaling:
+                    signaling = 1
+                state.append(np.hstack((coords[0],fov, [signaling])))
+            else:
+                state.append(np.hstack((coords[0],fov)))
         else:
             state.append(coords[0])
-        if self.n_agents > 1 and self.enable_signaling:
-            for agent_coord in coords[1:]:
-                vec = np.hstack((agent_coord, np.array(self.fov_buffer).flatten()))
-                state.append(vec)
+        for idx in range(1,self.n_agents):
+            if self.enable_signaling:
+                if lead_hive and self.is_signaling and self._in_hive(idx):
+                    self.communication_vec[idx] = np.array(self.fov_buffer).flatten()
+                vec = np.hstack((coords[idx], self.communication_vec[idx]))
+            else:
+                vec = coords[idx]
+            state.append(vec)
         if self.n_agents == 1:
             state = np.array(state)
         self.state = state
@@ -91,24 +118,28 @@ class BeeEnv(gym.Env, EzPickle):
     def reset(self):
         self.positions = np.array([HIVE for i in range(self.n_agents)])
         self.pollen = np.array([False] * self.n_agents)
+        self.communication_vec = np.zeros((self.n_agents, self.n_frames * 2))
+        self.is_signaling = False
         self.t = 0
         if self.fov:
             self._place_flower()
         self._compute_state()
         
         if self.logging:
-            self.log = []
+            # reset log
+            self.log = [self.positions.copy()]
         return self.state
     
     def action_space_sample(self):
-        return [agent_action_space.sample() for agent_action_space in self.action_space]
+        return np.array([agent_action_space.sample() for agent_action_space in self.action_space])
     
-    def step(self, action):
+    def step(self, action_):
         # if (not np.isin(action, np.arange(8))):
         #     print('Error: The argument A must be an integer from 0-7, indicating which action was selected.')
-        self.positions += self.directions[action]
+        action = np.array(action_)
+        self.positions += self.directions[np.mod(action, self.n_movements)]
         self.positions = np.clip(self.positions, self.min_position, self.max_position)
-
+        self.is_signaling = action[0] > self.n_movements #np.where(action > self.n_movements)
         self.pollen[(self.positions[:, 0] == self.flower[0]) & (self.positions[:, 1] == self.flower[1])] = True
         done = self.pollen.all() or self.t == self._max_episode_steps
         reward = -1
